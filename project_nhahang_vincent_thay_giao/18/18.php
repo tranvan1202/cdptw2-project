b7d803ebbb01c166161dc44997b35fe8b0f3a2a4
tdc-students <cuongtk@tdc.edu.vn>
2019-10-16 10:25:05 +0700

    full-module 18

--- project_nhahang_vincent_thay_giao/18/18.php
+++ project_nhahang_vincent_thay_giao/18/18.php
@@ -15,17 +15,14 @@ $less->compileFile('less/18.less', 'css/18.css');
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <title>18</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
     <link href="css/18.css" rel="stylesheet" type="text/css"/>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-
+     <script src="js/18.js"></script>
 </head>
 <body>
     <?php include '../18/18-content.php'; ?>
