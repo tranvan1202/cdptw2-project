82b54b09d17823013aad66a5b874b93a2d7316ca
tranvan1202 <bshine619@gmail.com>
2019-10-19 15:44:55 +0700

    created module 14

---
+++ project_nhahang_vincent_thay_giao/14/14-content.php
@@ -0,0 +1,43 @@
+<?php
+$url_host = $_SERVER['HTTP_HOST'];
+
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-14">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-xs-3">
+        <div class="widget_filter">
+          <h6 class="widget_filter_title">TAGS</h6> 
+
+          <div class="widget_filter_list">
+            <ul>
+              <li>
+                <div class="row"> 
+                  <div class="col-md-9 col-xs-9 filter_list">
+                    <a href="#" class="filter_name">Burgers</a>
+                    <a href="#" class="filter_name">Dessert</a>
+                    <a href="#" class="filter_name">Drinks</a>
+                    <a href="#" class="filter_name">Pasta</a>
+                    <a href="#" class="filter_name">Pizza</a>
+                    <a href="#" class="filter_name">Salad</a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
